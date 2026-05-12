4a72a840ffad669bd78e378059041d39520f5a72
sbsteacher <id@gmail.com>
2026-05-12 10:44:04 +0900

    배포 디렉토리 환경변수 설정-2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') }
-  }  
+export default defineConfig(({ mode }) => {
+  console.log('mode: ', mode);
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    build: {
+      // .env 파일의 VITE_OUT_DIR 값을 사용하고, 없으면 기본값 'dist'를 사용합니다.
+      outDir: env.VITE_OUT_DIR || 'dist',
+      // 폴더가 비어있지 않아도 빌드 시 삭제 후 생성 (선택 사항)
+      emptyOutDir: true,
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: { '@': path.resolve(__dirname, './src') }
+    }  
+  }
 })
